c0dc40bfb750de495956ae0db447eb5752b31ccf
Carlos Eduardo <dev.carloseddie@outlook.com>
2024-04-23 15:57:15 -0300

    changed footer text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="footer paragraph">
-        Developed by Carlos | 2023 - Fictional non-commercial project.
+        Developed by Carlos | 2024 - Fictional non-commercial project.
     </footer>
 </template>
 
